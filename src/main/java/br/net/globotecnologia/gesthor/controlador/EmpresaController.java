7a92903dfab70487d0ca632cba30e3300bdbdc9c
7950 <marciow.ferreira@bancoamazonia.com.br>
2018-09-26 16:14:42 -0300

    merging com oq tinha feito em casa

--- src/main/java/br/net/globotecnologia/gesthor/controlador/EmpresaController.java
+++ src/main/java/br/net/globotecnologia/gesthor/controlador/EmpresaController.java
@@ -1,15 +1,41 @@
 package br.net.globotecnologia.gesthor.controlador;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import br.net.globotecnologia.gesthor.modelo.Contrato;
+import br.net.globotecnologia.gesthor.modelo.Empresa;
+import br.net.globotecnologia.gesthor.repositorio.EmpresaRepository;
 
 @Controller
-public class EmpresaController extends RootController{
+public class EmpresaController{
 	
-	@Override
+	@Autowired
+	private EmpresaRepository empresaRepository;
+	
 	@GetMapping("/empresas/lista")
 	public String lista() {
-		return super.lista();
+		return "empresa/lista";
 	}
 
+	@GetMapping("/empresas/novo")
+	public String novo(Empresa empresa){
+		return "empresa/novo";
+	}
+	
+	@PostMapping("/empresas/salvar")
+	public String salvar(Empresa empresa) {
+		empresaRepository.save(empresa);
+		return "redirect:/empresas/lista";
+	}
+	
+	@ModelAttribute("empresas")
+	public List<Empresa> getListaEmpresas(){
+		return (List<Empresa>) empresaRepository.findAll();
+	}
 }
